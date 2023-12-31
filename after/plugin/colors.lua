c1d696c812b78cef62fe6bc16dd58c13dc33af12
Pratush Rai <pratushrai0309@gmail.com>
2023-12-31 20:03:30 +0530

    Telescope.nvim

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyLife()
-	color = color or "tokyonight-moon"
+	local color = color or "tokyonight-night"
 	vim.cmd.colorscheme(color)
 	--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- lua/pratush/remap.lua
+++ lua/pratush/remap.lua
@@ -1,8 +1,18 @@
 local builtin = require("telescope.builtin")
+
+-- Function to launch mason
+local function mason()
+	vim.cmd "Mason"	
+end
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.keymap.set("n", "<leader>q", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>e", builtin.find_files, {})
+vim.keymap.set("n", "<leader>m", mason, {})
 vim.keymap.set("n", "<leader>s", function ()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
+end, {})
+vim.keymap.set("n", "<leader>g", function ()
+	builtin.live_grep()
+end, {})
